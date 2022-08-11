7c407486329479ea8f0673fe767051d3ae6bddf4
Marta Litvinchuk <martalitvinchuk90@gmail.com>
2022-08-11 18:14:29 +0300

    prettier

---
+++ js-start/js/script.js
@@ -0,0 +1,24 @@
+
+"use strict"
+
+console.log('Я вчу js');
+
+
+//infinity
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+//undefined
+let userName;
+console.log(typeof userName);
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
+
+console.log('35' + - "22");
+
+console.log('35' * "22");
+
+console.log(!false && 11 || 18 && !'');
+
+
